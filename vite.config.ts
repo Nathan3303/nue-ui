eff3109e9d23cf35ffbfe8d2c5a28a74b8d75b01
nathan33 <lee1928@outlook.com>
2024-05-09 02:07:49 +0800

    commit.

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: "./packages/core/index.ts",
+            name: "nue-ui",
+            formats: ["es"],
+            fileName: () => "nueui.js",
+        },
+        outDir: "./dist",
+        target: "es2015",
+        copyPublicDir: false,
+        emptyOutDir: false,
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+    },
+    plugins: [vue()],
+});
