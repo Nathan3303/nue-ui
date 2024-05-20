40655e2b27974508f05f300ff619a73f1b5c3b03
nathan33 <lee1928@outlook.com>
2024-05-20 14:17:06 +0800

    修复 docs 无法正常部署的问题

--- packages/core/types.ts
+++
@@ -1,1 +0,0 @@
-export type * from "@nue-ui/components";
--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,7 +1,10 @@
 import DefaultTheme from "vitepress/theme";
-import nueui from "../../../core/index";
+import NueUI from "nue-ui";
+import "nue-ui/dist/index.css";
 
 export default {
     extends: DefaultTheme,
-    enhanceApp({ app }) { app.use(nueui) },
+    enhanceApp({ app }) {
+        app.use(NueUI);
+    },
 };
