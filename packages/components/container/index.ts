71f58d8c85f78e6899b109ebd836d0db1df98b10
nathan33 <lee1928@outlook.com>
2024-05-21 11:25:05 +0800

    导出 container 组件类型

--- packages/components/container/index.ts
+++ packages/components/container/index.ts
@@ -3,8 +3,14 @@ import Container from "./container.vue";
 import Main from "./main.vue";
 import Footer from "./footer.vue";
 import { withInstall } from "@nue-ui/utils";
+import type {
+    ContainerPropsType,
+    HeaderPropsType,
+    MainPropsType,
+} from "./types";
 
 export const NueHeader = withInstall(Header);
 export const NueContainer = withInstall(Container);
 export const NueMain = withInstall(Main);
 export const NueFooter = withInstall(Footer);
+export type { ContainerPropsType, HeaderPropsType, MainPropsType };
