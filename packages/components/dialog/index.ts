6d42ed2de60a80515a520594fc097975941b55aa
nathan33 <lee1928@outlook.com>
2024-05-21 11:25:37 +0800

    导出 dialog 组件类型

--- packages/components/dialog/index.ts
+++ packages/components/dialog/index.ts
@@ -1,4 +1,6 @@
 import { withInstall } from "@nue-ui/utils";
 import Dialog from "./dialog.vue";
+import type { DialogPropsType, DialogEmitsType } from "./types";
 
 export const NueDialog = withInstall(Dialog);
+export type { DialogPropsType, DialogEmitsType };
