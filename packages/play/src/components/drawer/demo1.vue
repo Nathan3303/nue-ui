73c66238a8c9fec608b5ea6dd5f1117a9fdf20ab
nathan33 <lee1928@outlook.com>
2024-05-21 11:30:22 +0800

    增加 drawer 组件的 PlayGround Demo

---
+++ packages/play/src/components/drawer/demo1.vue
@@ -0,0 +1,29 @@
+<template>
+    <nue-div>
+        <nue-button @click="drawer1Visible = true">Open drawer1</nue-button>
+        <nue-button @click="drawer2Visible = true">Open drawer2</nue-button>
+    </nue-div>
+    <nue-drawer
+        v-model:visible="drawer1Visible"
+        span="42%"
+        min-span="360px"
+        title="Drawer1">
+        <nue-text tag="p">drawer1 content</nue-text>
+        <nue-button @click="drawer2Visible = true">Open drawer2</nue-button>
+    </nue-drawer>
+    <nue-drawer
+        v-model:visible="drawer2Visible"
+        span="36%"
+        min-span="240px"
+        title="Drawer2"
+        open-from="left">
+        drawer2 content
+    </nue-drawer>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const drawer1Visible = ref(false);
+const drawer2Visible = ref(false);
+</script>
--- packages/play/src/views/feedback-view.vue
+++ packages/play/src/views/feedback-view.vue
@@ -1,9 +1,17 @@
 <template>
-    <nue-div vertical>
-        <dialog-demo1></dialog-demo1>
+    <nue-div vertical :divider="{}">
+        <nue-div vertical>
+            <nue-text size="large">Dialog</nue-text>
+            <dialog-demo1></dialog-demo1>
+        </nue-div>
+        <nue-div vertical>
+            <nue-text size="large">Drawer</nue-text>
+            <drawer-demo1></drawer-demo1>
+        </nue-div>
     </nue-div>
 </template>
 
 <script setup lang="ts">
 import DialogDemo1 from "../components/dialog/demo1.vue";
+import DrawerDemo1 from "../components/drawer/demo1.vue";
 </script>
