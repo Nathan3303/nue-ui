858b0d772e500c1115df7cd02481385f7bb2c826
nathan33 <lee1928@outlook.com>
2024-06-03 15:00:23 +0800

    导出 Input 组件的所有类型

--- packages/components/input/index.ts
+++ packages/components/input/index.ts
@@ -1,6 +1,23 @@
 import { withInstall } from "@nue-ui/utils";
 import Input from "./input.vue";
 import Textarea from "./textarea.vue";
+import type {
+    InputPropsType,
+    InputType,
+    InputCounterType,
+    InputEmitsType,
+    InputShapeType,
+    TextareaPropsType,
+} from "./types";
 
 export const NueInput = withInstall(Input);
 export const NueTextarea = withInstall(Textarea);
+
+export type {
+    InputPropsType,
+    InputType,
+    InputCounterType,
+    InputEmitsType,
+    InputShapeType,
+    TextareaPropsType,
+};
