dda587bc9aacbc73411a3bddf5c82c3b9cdcf5eb
nathan33 <lee1928@outlook.com>
2024-06-07 11:46:36 +0800

    重构 ButtonGroup 组件部分逻辑

--- packages/components/button-group/button-group.vue
+++ packages/components/button-group/button-group.vue
@@ -1,22 +1,32 @@
 <template>
-    <div class="nue-button-group">
+    <div :class="classes">
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
-import "./button-group.css";
-import type { ButtonGroupPropsType, ButtonGroupCtxType } from "./types";
+import { provide, reactive, computed } from "vue";
+import type { ButtonGroupProps, ButtonGroupCtxType } from "./types";
 import { BUTTON_GROUP_CTX_KEY } from "./constants";
-import { provide, reactive } from "vue";
+import "./button-group.css";
+import { parseTheme } from "@nue-ui/utils";
 
 defineOptions({ name: "NueButtonGroup" });
 
-const props = defineProps<ButtonGroupPropsType>();
+const props = defineProps<ButtonGroupProps>();
 
 const ButtonGroupCtx = reactive<ButtonGroupCtxType>({
     size: props.size,
     disabled: props.disabled,
+});
+
+const classes = computed(() => {
+    const { theme } = props;
+    let list: string[] = [];
+    const prefix = "nue-button-group";
+    list.push(prefix);
+    if (theme) list = list.concat(parseTheme(theme, prefix));
+    return list;
 });
 
 provide(BUTTON_GROUP_CTX_KEY, ButtonGroupCtx);
