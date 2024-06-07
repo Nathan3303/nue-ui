05f66ef02d117610a4de062e9333bd37d728414e
nathan33 <lee1928@outlook.com>
2024-06-07 11:47:08 +0800

    重构 Collapse 部分逻辑与样式

--- packages/components/collapse/collapse.vue
+++ packages/components/collapse/collapse.vue
@@ -1,22 +1,31 @@
 <template>
-    <div class="nue-collapse">
+    <div :class="classes">
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import type { CollapseEmitType, CollapsePropsType } from "./types";
 import { useCollapse } from "./collapse";
+import { parseTheme } from "@nue-ui/utils";
 
 defineOptions({ name: "NueCollapse" });
 
+const emit = defineEmits<CollapseEmitType>();
 const props = withDefaults(defineProps<CollapsePropsType>(), {
     accordion: false,
 });
 
-const emit = defineEmits<CollapseEmitType>();
-
 const { activedItems, pushActivedItem } = useCollapse(props, emit);
+
+const classes = computed(() => {
+    const { theme } = props;
+    let list: string[] = [];
+    const prefix = "nue-collapse";
+    if (theme) list = list.concat(parseTheme(theme, prefix));
+    return list;
+});
 
 defineExpose({ activedItems, pushActivedItem });
 </script>
