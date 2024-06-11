1d7f21570a273b77336d7eb236dd69054d79d95d
nathan33 <lee1928@outlook.com>
2024-06-11 17:28:25 +0800

    更新部分组件的 playground demo

--- packages/play/src/components/link/demo1.vue
+++ packages/play/src/components/link/demo1.vue
@@ -3,7 +3,7 @@
         <nue-text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
             possimus hic eos minus, Go
-            <nue-link href="https://www.baidu.com"></nue-link>
+            <nue-link href="https://www.baidu.com" icon="connection"></nue-link>
             qui ipsa numquam. Vel sequi, pariatur ab dolor reprehenderit
             sapiente facilis, molestiae quae doloribus a voluptate consectetur!
         </nue-text>
