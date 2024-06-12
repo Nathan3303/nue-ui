e772f6e457514b40d565368dc94cee130bab70ca
nathan33 <lee1928@outlook.com>
2024-06-12 15:56:52 +0800

    新增 Select 组件的 playground demo

---
+++ packages/play/src/components/select/demo1.vue
@@ -0,0 +1,14 @@
+<template>
+    <nue-div align="center">
+        <nue-select></nue-select>
+        <nue-select size="small">
+            <li data-executeid="10">10</li>
+            <li data-executeid="20">20</li>
+            <li data-executeid="30">30</li>
+        </nue-select>
+    </nue-div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
