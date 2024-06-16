c81f68e227b8441f24888e0eac08d19e1581e525
nathan33 <lee1928@outlook.com>
2024-06-16 13:03:53 +0800

    更新 Select 组件的 playground demo

--- packages/play/src/components/select/demo1.vue
+++ packages/play/src/components/select/demo1.vue
@@ -5,15 +5,29 @@
         <nue-select size="large"></nue-select>
     </nue-div>
     <nue-divider></nue-divider>
-    <nue-div align="center">
-        <nue-select size="small" @change="handleChange">
-            <li :data-executeid="24">24</li>
-            <li :data-executeid="36">36</li>
-            <li :data-executeid="48">48</li>
-        </nue-select>
-        <nue-avatar
-            src="https://img.icons8.com/color/48/000000/user-male.png"
-            :size="size"></nue-avatar>
+    <nue-div align="center" :divider="{}">
+        <nue-div align="center" width="fit-content">
+            <nue-select size="small" v-model="size1">
+                <nue-select-option label="24px" value="24px" />
+                <nue-select-option label="36px" value="36px" />
+                <nue-select-option label="48px" value="48px" />
+            </nue-select>
+            →
+            <nue-avatar
+                src="https://img.icons8.com/color/48/000000/user-male.png"
+                :size="size1"></nue-avatar>
+        </nue-div>
+        <nue-div align="center" width="fit-content">
+            <nue-select size="small" @change="handleSize2Change">
+                <li :data-executeid="24">24</li>
+                <li :data-executeid="36">36</li>
+                <li :data-executeid="48">48</li>
+            </nue-select>
+            →
+            <nue-avatar
+                src="https://img.icons8.com/color/48/000000/user-male.png"
+                :size="size2"></nue-avatar>
+        </nue-div>
     </nue-div>
     <nue-divider></nue-divider>
     <nue-div>
@@ -28,11 +42,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const size = ref("24px");
+const size1 = ref("24px");
+const size2 = ref("24px");
 
-function handleChange(value: number) {
-    size.value = `${value}px`;
-}
+// const handleSize1Change = (value: number) => (size1.value = `${value}px`);
+const handleSize2Change = (value: number) => (size2.value = `${value}px`);
 </script>
-
-<style scoped></style>
