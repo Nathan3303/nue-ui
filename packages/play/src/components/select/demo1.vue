88d8e7eaedf8c2dd6a95ff3c30fb2790afb790d6
nathan33 <lee1928@outlook.com>
2024-06-30 07:25:29 +0800

    更新 Select 组件的 playground demo

--- packages/play/src/components/select/demo1.vue
+++ packages/play/src/components/select/demo1.vue
@@ -1,50 +1,28 @@
 <template>
     <nue-div align="center">
-        <nue-select size="small"></nue-select>
-        <nue-select></nue-select>
-        <nue-select size="large"></nue-select>
+        <nue-select placeholder="Select an option"></nue-select>
+        <nue-select placeholder="Select an option" disabled></nue-select>
     </nue-div>
-    <nue-divider></nue-divider>
-    <nue-div align="center" :divider="{}">
+    <nue-div align="center">
         <nue-div align="center" width="fit-content">
-            <nue-select size="small" v-model="size1">
-                <nue-select-option label="24px" value="24px" />
-                <nue-select-option label="36px" value="36px" />
-                <nue-select-option label="48px" value="48px" />
+            <nue-select
+                v-model="size1"
+                placeholder="Select to change the avatar size">
+                <nue-select-option label="24px" :value="24" />
+                <nue-select-option label="36px" :value="36" />
+                <nue-select-option label="48px" :value="48" />
+                <nue-select-option label="56px" :value="56" disabled />
             </nue-select>
             →
             <nue-avatar
                 src="https://img.icons8.com/color/48/000000/user-male.png"
-                :size="size1"></nue-avatar>
+                :size="`${size1}px`"></nue-avatar>
         </nue-div>
-        <nue-div align="center" width="fit-content">
-            <nue-select size="small" @change="handleSize2Change">
-                <li :data-executeid="24">24</li>
-                <li :data-executeid="36">36</li>
-                <li :data-executeid="48">48</li>
-            </nue-select>
-            →
-            <nue-avatar
-                src="https://img.icons8.com/color/48/000000/user-male.png"
-                :size="size2"></nue-avatar>
-        </nue-div>
-    </nue-div>
-    <nue-divider></nue-divider>
-    <nue-div>
-        <nue-select disabled>
-            <li>Option 1</li>
-            <li>Option 2</li>
-            <li>Option 3</li>
-        </nue-select>
     </nue-div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const size1 = ref("24px");
-const size2 = ref("24px");
-
-// const handleSize1Change = (value: number) => (size1.value = `${value}px`);
-const handleSize2Change = (value: number) => (size2.value = `${value}px`);
+const size1 = ref(24);
 </script>
