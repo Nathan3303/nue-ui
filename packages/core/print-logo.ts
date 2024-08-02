148147b577ae8a0419c67eb58303bd6ff3c21606
nathan33 <lee1928@outlook.com>
2024-08-02 21:08:18 +0800

    打包优化

---
+++ packages/core/print-logo.ts
@@ -0,0 +1,26 @@
+export default function () {
+    if (PROD) {
+        console.info(
+            `%c NueUI is running in production mode.`,
+            `
+                background: linear-gradient(
+                    to right,
+                    #ff2400, #ff5733, #ff8300, #ffaa00,
+                    #ffd14d, #00e436, #00c9ff, #4d80ff,
+                    #8333ff, #aa00ff, #ff00cc
+                );
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-size: 12px;
+                line-height: 1;
+                font-family: monospace;
+                font-weight: bold;
+            `
+        );
+    } else if (DEV) {
+        console.info(
+            `%c NueUI is running in development mode.`,
+            `font-size: 12px; font-weight: bold;`
+        );
+    }
+}
