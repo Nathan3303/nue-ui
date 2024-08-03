95c3e040b8e9cd46c94ab5f1f794b4399818a27d
nathan33 <lee1928@outlook.com>
2024-08-03 15:34:07 +0800

    实现 Main 组件侧栏拉伸功能

--- packages/components/container/main.vue
+++ packages/components/container/main.vue
@@ -1,23 +1,23 @@
 <template>
-    <main class="nue-main" :style="style">
-        <!-- Aside -->
-        <div v-if="$slots.aside" class="nue-main__aside">
+    <main :class="classes" :style="style">
+        <aside v-if="$slots.aside" ref="asideRef" class="nue-main__aside">
             <slot name="aside"></slot>
-        </div>
-        <!-- Divider -->
-        <!-- <nue-divider direction="vertical" /> -->
-        <!-- Content -->
+            <i
+                v-show="allowResizeAside"
+                ref="resizerRef"
+                class="nue-main__aside__resizer"
+                @mousedown="handleMouseDown"
+                @dblclick="handleDoubleClick"></i>
+        </aside>
         <div v-if="$slots.content" class="nue-main__content">
             <slot name="content"></slot>
         </div>
-        <!-- Default slot -->
         <slot></slot>
     </main>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-// import { NueDivider } from "../divider";
+import { computed, ref } from "vue";
 import type { MainPropsType } from "./types";
 import "./main.css";
 
@@ -25,11 +25,58 @@ defineOptions({ name: "NueMain" });
 
 const props = withDefaults(defineProps<MainPropsType>(), {});
 
+const asideRef = ref<HTMLElement>();
+const resizerRef = ref<HTMLElement>();
+const isResizing = ref(false);
+let originalX = 0;
+let originalWidth = 0;
+
 const style = computed(() => {
-    const { asideWidth } = props;
+    const {
+        allowResizeAside,
+        asideWidth,
+        asideMinWidth,
+        asideMaxWidth,
+        contentPadding,
+    } = props;
     return {
-        "--aside-width": asideWidth,
-        "--content-padding": props.contentPadding,
+        "--aside-width": allowResizeAside ? void 0 : asideWidth,
+        "--aside-min-width": asideMinWidth,
+        "--aside-max-width": asideMaxWidth,
+        "--content-padding": contentPadding,
     };
 });
+
+const classes = computed(() => {
+    const list = [];
+    list.push("nue-main");
+    if (isResizing.value) list.push("nue-main--resizing");
+    return list;
+});
+
+const handleMouseMove = (event: MouseEvent) => {
+    const { clientX: boundX } = event;
+    document.body.style.cursor = "col-resize";
+    const newWidth = boundX - originalX + originalWidth;
+    asideRef.value!.style.width = newWidth + "px";
+};
+
+const handleMouseUp = () => {
+    document.documentElement.removeEventListener("mousemove", handleMouseMove);
+    document.documentElement.removeEventListener("mouseup", handleMouseUp);
+    isResizing.value = false;
+    document.body.style.cursor = "default";
+};
+
+const handleMouseDown = (event: MouseEvent) => {
+    isResizing.value = true;
+    originalX = event.clientX;
+    originalWidth = parseInt(window.getComputedStyle(asideRef.value!).width);
+    document.documentElement.addEventListener("mousemove", handleMouseMove);
+    document.documentElement.addEventListener("mouseup", handleMouseUp);
+};
+
+const handleDoubleClick = () => {
+    asideRef.value!.style.width = "200px";
+};
 </script>
