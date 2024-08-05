e829ee907105017aeeb67adb4e6e295837fa3ad2
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-08-05 09:27:25 +0800

    新增组件 Progress

---
+++ packages/components/progress/types.ts
@@ -0,0 +1,17 @@
+import type { GlobalProps } from "@nue-ui/utils";
+
+export type ProgressType = "line" | "circle" | "dashboard";
+
+export interface ProgressProps extends GlobalProps {
+    type?: ProgressType;
+    percentage?: number;
+    color?: string | string[];
+    strokeWidth?: number;
+    formatter?: Function;
+    showInnerText?: boolean;
+    scale?: number;
+}
+
+export type ProgressEmits = {
+    (event: "full"): void;
+};
