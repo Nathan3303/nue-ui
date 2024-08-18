e72121d05986962523774db12157447b8d165a16
Nathan3303 <Lee1928@outlook.com>
2024-08-18 17:04:25 +0800

    新增 Switch 组件的 playground demo

--- packages/play/src/components/switch.vue
+++ packages/play/src/components/switch.vue
@@ -3,9 +3,39 @@
         <nue-switch size="small"></nue-switch>
         <nue-switch></nue-switch>
         <nue-switch size="large"></nue-switch>
+        <nue-switch show-text></nue-switch>
+        <nue-switch show-text active-text="开" inactive-text="关"></nue-switch>
+        <nue-switch
+            show-text
+            active-text="开"
+            inactive-text="关"
+            v-model="isActive"
+            :loading="isLoading"
+            :before-switch="handleBeforeSwitch"></nue-switch>
+        <nue-switch theme="custom"></nue-switch>
     </nue-div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
 
-<style scoped></style>
+const isActive = ref(false);
+const isLoading = ref(false);
+
+const handleBeforeSwitch = async () => {
+    isLoading.value = true;
+    const result = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, 1000);
+    });
+    isLoading.value = false;
+    return result;
+};
+</script>
+
+<style scoped>
+.nue-switch--custom {
+    --background-active-color: var(--primary-color-a200);
+}
+</style>
