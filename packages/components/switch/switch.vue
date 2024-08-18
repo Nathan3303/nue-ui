8a3725dd35a97f1e84e87e572f90477dde095460
Nathan3303 <Lee1928@outlook.com>
2024-08-18 17:02:59 +0800

    完善 Switch 组件基本功能

--- packages/components/switch/switch.vue
+++ packages/components/switch/switch.vue
@@ -1,43 +1,84 @@
 <template>
-    <div class="nue-switch" :class="classes" @click.stop="handleClick">
-        <div class="nue-switch__circle"></div>
+    <div :class="classes" @click.stop="handleClick" ref="switchRef">
+        <div class="nue-switch__circle">
+            <nue-icon
+                class="nue-switch__loading-icon"
+                v-if="loading"
+                name="loading"
+                spin></nue-icon>
+        </div>
+        <div class="nue-switch__text" v-if="showText">
+            <slot>{{ state ? activeText : inactiveText }}</slot>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, watch } from "vue";
 import { parseTheme } from "@nue-ui/utils";
+import { isFunction } from "lodash-es";
+import { NueIcon } from "../icon";
 import type { NueSwitchProps, NueSwitchEmits } from "./types";
 import "./switch.css";
 
 defineOptions({ name: "NueSwitch" });
 const props = withDefaults(defineProps<NueSwitchProps>(), {
     disabled: false,
-    theme: "default",
+    showText: false,
+    loading: false,
+    activeText: "I",
+    inactiveText: "O",
 });
 const emit = defineEmits<NueSwitchEmits>();
 
 const state = ref(false);
+const switchRef = ref<HTMLDivElement>();
 
 const classes = computed(() => {
-    const { size, theme } = props;
-    const list: string[] = [];
+    const { size, theme, disabled } = props;
+    let list: string[] = [];
     const prefix = "nue-switch";
     list.push(prefix);
-    if (theme) list.concat(parseTheme(theme, prefix));
-    if (state.value) list.push("nue-switch--actived");
-    if (size) list.push(`nue-switch--${size}`);
+    if (theme) list = list.concat(parseTheme(theme, prefix));
+    if (state.value) list.push(`${prefix}--actived`);
+    if (size) list.push(`${prefix}--${size}`);
+    if (disabled) list.push(`${prefix}--disabled`);
     return list;
 });
 
-const handleClick = () => {
+const handleClick = async () => {
+    const { loading, disabled, beforeSwitch } = props;
+    if (loading || disabled) return;
+    if (isFunction(beforeSwitch)) {
+        try {
+            const result = await beforeSwitch(state.value);
+            if (!result) return;
+        } catch (error) {
+            return;
+        }
+    }
+    handleSwitch();
+};
+
+const handleSwitch = () => {
     state.value = !state.value;
     emit("update:modelValue", state.value);
+    emit("change", state.value);
+};
+
+const handleSetStateWidth = () => {
+    requestIdleCallback(() => {
+        const cw = switchRef.value?.clientWidth;
+        switchRef.value?.style.setProperty("--state-width", `${cw}px`);
+    });
 };
 
 watch(
     () => props.modelValue,
-    (newValue) => (state.value = newValue),
+    (newValue) => {
+        state.value = newValue;
+        handleSetStateWidth();
+    },
     { immediate: true }
 );
 </script>
--- packages/components/switch/types.ts
+++ packages/components/switch/types.ts
@@ -4,8 +4,14 @@ export interface NueSwitchProps extends GlobalProps {
     modelValue?: boolean;
     size?: "small" | "large";
     disabled?: boolean;
+    loading?: boolean;
+    showText?: boolean;
+    activeText?: string;
+    inactiveText?: string;
+    beforeSwitch?: (value: boolean) => Promise<boolean | void> | boolean | void;
 }
 
 export type NueSwitchEmits = {
     (event: "update:modelValue", value: boolean): void;
+    (event: "change", value: boolean): void;
 };
