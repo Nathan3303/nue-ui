3c0649b1b51b5c122097ce18a576bb8dbf032213
Nathan3303 <Lee1928@outlook.com>
2024-08-28 01:51:03 +0800

    抽离 Tooltip 组件逻辑，复用为 usePopper 和 usePopperController

--- packages/components/tooltip/tooltip.vue
+++ packages/components/tooltip/tooltip.vue
@@ -4,10 +4,6 @@
         @mouseenter="handleShow"
         @mouseleave="handleHide"
         ref="tooltipWrapperRef">
-        <div
-            v-if="visible"
-            class="nue-tooltip__buffer"
-            :data-direction="placementInfo.direction"></div>
         <slot></slot>
         <teleport to="#NueTooltipPool">
             <div
@@ -18,6 +14,9 @@
                 @mouseenter="handleShow"
                 @mouseleave="handleHide"
                 ref="tooltipRef">
+                <div
+                    class="nue-tooltip__padding"
+                    :data-direction="placementInfo.direction"></div>
                 <slot name="content">
                     <span class="nue-tooltip__text">{{ content }}</span>
                 </slot>
@@ -27,8 +26,9 @@
 </template>
 
 <script setup lang="ts">
-import { computed, nextTick, reactive, ref } from "vue";
+import { computed, ref } from "vue";
 import { useTooltipPool } from "./use-tooltip-pool";
+import { usePopper, usePopperController } from "@nue-ui/hooks";
 import { parseTheme } from "@nue-ui/utils";
 import type { NueTooltipProps, NueTooltipEmits } from "./types";
 import "./tooltip.css";
@@ -40,30 +40,27 @@ const props = withDefaults(defineProps<NueTooltipProps>(), {
 });
 const emit = defineEmits<NueTooltipEmits>();
 
-const { activeTooltipPool, deactiveTooltipPool } = useTooltipPool();
-
 const visible = ref(false);
-const rectInfo = reactive({
-    width: 0,
-    height: 0,
-    wrapperX: 0,
-    wrapperY: 0,
-    wrapperWidth: 0,
-    wrapperHeight: 0,
-});
-const placementBuffer = ref(props.placement);
 const tooltipWrapperRef = ref<HTMLDivElement>();
 const tooltipRef = ref<HTMLDivElement>();
-const hideTimer = ref<number | null>();
+
+const { activeTooltipPool, deactiveTooltipPool } = useTooltipPool();
+const { placement, calculatePosition } = usePopper(
+    tooltipWrapperRef,
+    tooltipRef,
+    {
+        placement: props.placement,
+    }
+);
+const { show, hide } = usePopperController(visible);
 
 const tooltipClasses = computed(() => {
     const { theme, size } = props;
-    const placement = placementBuffer.value;
     const prefix = "nue-tooltip";
     let list: string[] = [prefix];
     if (theme) list = list.concat(parseTheme(theme, prefix));
     if (size) list.push(`${prefix}--${size}`);
-    if (placement) list.push(`${prefix}--${placement}`);
+    if (placement.value) list.push(`${prefix}--${placement.value}`);
     return list;
 });
 
@@ -75,164 +72,30 @@ const tooltipStyles = computed(() => {
 });
 
 const placementInfo = computed(() => {
-    const placement = placementBuffer.value;
-    const splited = placement.split("-");
+    const splited = placement.value.split("-");
     return {
         direction: splited[0] || "top",
         alignment: splited[1] || "center",
     };
 });
 
-const getRectInfo = () => {
-    const BCR = tooltipRef.value?.getBoundingClientRect();
-    const wrapperBCR = tooltipWrapperRef.value?.getBoundingClientRect();
-    rectInfo.width = BCR?.width || 0;
-    rectInfo.height = BCR?.height || 0;
-    rectInfo.wrapperX = wrapperBCR?.x || 0;
-    rectInfo.wrapperY = wrapperBCR?.y || 0;
-    rectInfo.wrapperWidth = wrapperBCR?.width || 0;
-    rectInfo.wrapperHeight = wrapperBCR?.height || 0;
-};
-
-const updatePlacementBuffer = (
-    direction: "top" | "bottom" | "left" | "right"
-) => {
-    const placement = placementBuffer.value;
-    const splited = placement.split("-");
-    splited[0] = direction;
-    const newPlacement = splited.join("-") || "top-center";
-    placementBuffer.value = (newPlacement as NueTooltipProps["placement"])!;
-};
-
-const checkOverflow = () => {
-    const placement = placementBuffer.value;
-    const direction = placement.split("-")[0];
-    const { width, height, wrapperX, wrapperY, wrapperWidth, wrapperHeight } =
-        rectInfo;
-    switch (direction) {
-        case "top":
-            if (wrapperY - height < 0) {
-                updatePlacementBuffer("bottom");
-                return true;
-            }
-            break;
-        case "bottom":
-            if (wrapperY + wrapperHeight + height > window.innerHeight) {
-                updatePlacementBuffer("top");
-                return true;
-            }
-            break;
-        case "left":
-            if (wrapperX - width < 0) {
-                updatePlacementBuffer("right");
-                return true;
-            }
-            break;
-        case "right":
-            if (wrapperX + wrapperWidth + width > window.innerWidth) {
-                updatePlacementBuffer("left");
-                return true;
-            }
-            break;
-    }
-    return false;
-};
-
-const calculatePosition = () => {
-    const tooltipEl = tooltipRef.value!;
-    if (checkOverflow()) {
-        calculatePosition();
-        return;
-    }
-    const { width, height, wrapperX, wrapperY, wrapperWidth, wrapperHeight } =
-        rectInfo;
-    const placement = placementBuffer.value;
-    switch (placement) {
-        case "top-center":
-            tooltipEl.style.left = `${
-                wrapperX + wrapperWidth / 2 - width / 2
-            }px`;
-            tooltipEl.style.top = `${wrapperY - height - 8}px`;
-            break;
-        case "top-start":
-            tooltipEl.style.left = `${wrapperX}px`;
-            tooltipEl.style.top = `${wrapperY - height - 8}px`;
-            break;
-        case "top-end":
-            tooltipEl.style.left = `${wrapperX + wrapperWidth - width}px`;
-            tooltipEl.style.top = `${wrapperY - height - 8}px`;
-            break;
-        case "bottom-center":
-            tooltipEl.style.left = `${
-                wrapperX + wrapperWidth / 2 - width / 2
-            }px`;
-            tooltipEl.style.top = `${wrapperY + wrapperHeight + 8}px`;
-            break;
-        case "bottom-start":
-            tooltipEl.style.left = `${wrapperX}px`;
-            tooltipEl.style.top = `${wrapperY + wrapperHeight + 8}px`;
-            break;
-        case "bottom-end":
-            tooltipEl.style.left = `${wrapperX + wrapperWidth - width}px`;
-            tooltipEl.style.top = `${wrapperY + wrapperHeight + 8}px`;
-            break;
-        case "left-center":
-            tooltipEl.style.left = `${wrapperX - width - 8}px`;
-            tooltipEl.style.top = `${
-                wrapperY + wrapperHeight / 2 - height / 2
-            }px`;
-            break;
-        case "left-start":
-            tooltipEl.style.left = `${wrapperX - width - 8}px`;
-            tooltipEl.style.top = `${wrapperY}px`;
-            break;
-        case "left-end":
-            tooltipEl.style.left = `${wrapperX - width - 8}px`;
-            tooltipEl.style.top = `${wrapperY + wrapperHeight - height}px`;
-            break;
-        case "right-center":
-            tooltipEl.style.left = `${wrapperX + wrapperWidth + 8}px`;
-            tooltipEl.style.top = `${
-                wrapperY + wrapperHeight / 2 - height / 2
-            }px`;
-            break;
-        case "right-start":
-            tooltipEl.style.left = `${wrapperX + wrapperWidth + 8}px`;
-            tooltipEl.style.top = `${wrapperY}px`;
-            break;
-        case "right-end":
-            tooltipEl.style.left = `${wrapperX + wrapperWidth + 8}px`;
-            tooltipEl.style.top = `${wrapperY + wrapperHeight - height}px`;
-            break;
-        default:
-            break;
-    }
-};
-
 const handleShow = () => {
-    if (hideTimer.value) {
-        clearTimeout(hideTimer.value);
-        hideTimer.value = null;
-        return;
-    }
-    placementBuffer.value = props.placement;
-    visible.value = true;
-    nextTick(() => {
-        getRectInfo();
-        calculatePosition();
+    show(void 0, () => {
+        calculatePosition(props.placement);
         activeTooltipPool();
     });
 };
 
 const handleHide = () => {
-    hideTimer.value = setTimeout(() => {
-        if (!tooltipRef.value) return;
-        tooltipRef.value.style.animationName = "slide-fade-out";
-        setTimeout(() => {
-            visible.value = false;
+    hide(
+        160,
+        () => {
+            if (!tooltipRef.value) return;
+            tooltipRef.value.style.animationName = "slide-fade-out";
+        },
+        () => {
             deactiveTooltipPool();
-        }, 160);
-        hideTimer.value = null;
-    }) as unknown as number;
+        }
+    );
 };
 </script>
