304ef12b2a4558eff4bd5fbf4adf5cc5bb4b7448
Nathan3303 <Lee1928@outlook.com>
2024-08-28 01:52:02 +0800

    更新 Dropdown 组件的 playground demo

--- packages/play/src/components/dropdown.vue
+++ packages/play/src/components/dropdown.vue
@@ -1,81 +1,61 @@
 <template>
     <nue-div align="center">
-        <nue-dropdown trigger="hover" text="Hover">
+        <nue-dropdown trigger-text="点击打开">
             <template #dropdown>
-                <li>trigger="hover"</li>
-                <li>text="Hover"</li>
+                <li class="nue-dropdown-item">下拉列表项 1</li>
+                <li class="nue-dropdown-item">下拉列表项 2</li>
             </template>
         </nue-dropdown>
-        <nue-dropdown trigger="click" text="Click">
+        <nue-dropdown trigger-text="空下拉列表"></nue-dropdown>
+        <nue-dropdown trigger-text="禁用状态" disabled></nue-dropdown>
+    </nue-div>
+    <nue-div align="center">
+        <nue-dropdown trigger-text="小型下拉框" size="small">
             <template #dropdown>
-                <li>trigger="click"</li>
-                <li>text="Click"</li>
+                <li class="nue-dropdown-item">下拉列表项 1</li>
+                <li class="nue-dropdown-item">下拉列表项 2</li>
+            </template>
+        </nue-dropdown>
+        <nue-dropdown trigger-text="大型下拉框" size="large">
+            <template #dropdown>
+                <li class="nue-dropdown-item">下拉列表项 1</li>
+                <li class="nue-dropdown-item">下拉列表项 2</li>
             </template>
         </nue-dropdown>
     </nue-div>
     <nue-div align="center">
-        <nue-dropdown text="Lefty" align="left">
+        <nue-dropdown trigger-text="位置：上-中" placement="top-center">
             <template #dropdown>
-                <li>trigger="click"</li>
-                <li>text="Lefty"</li>
-                <li>align="left"</li>
+                <li class="nue-dropdown-item">下拉列表项 1（长文本）</li>
             </template>
         </nue-dropdown>
-        <nue-dropdown text="Centered" align="center">
+        <nue-dropdown trigger-text="位置：下-中" placement="bottom-center">
             <template #dropdown>
-                <li>trigger="click"</li>
-                <li>text="Centered"</li>
-                <li>align="center"</li>
+                <li class="nue-dropdown-item">下拉列表项 1（长文本）</li>
             </template>
         </nue-dropdown>
-        <nue-dropdown text="Righty" align="right">
+        <nue-dropdown trigger-text="位置：下-右" placement="bottom-end">
             <template #dropdown>
-                <li>trigger="click"</li>
-                <li>text="Righty"</li>
-                <li>align="right"</li>
+                <li class="nue-dropdown-item">下拉列表项 1（长文本）</li>
             </template>
         </nue-dropdown>
     </nue-div>
     <nue-div align="center">
-        <nue-dropdown trigger="hover" @execute="handleExecute" hide-on-click>
-            <template #default>
-                <nue-div align="center" gap="8px">
-                    <nue-avatar
-                        src="https://picsum.photos/id/242/64/64"
-                        theme="rounded"
-                        size="28px" />
-                    <nue-button theme="pure">
-                        Username
-                        <template #append>
-                            <nue-icon
-                                class="state-icon"
-                                name="arrow-down"
-                                size="14px" />
-                        </template>
-                    </nue-button>
-                </nue-div>
-            </template>
+        <nue-dropdown
+            trigger-text="点击打开（选择后关闭）"
+            hide-on-clicked
+            @execute="handleExecute">
             <template #dropdown>
-                <li data-executeid="profile">My profile</li>
-                <li data-executeid="settings">Settings</li>
-                <li data-executeid="logout">Logout</li>
+                <li data-executeid="Option 1">下拉列表项 1</li>
+                <li data-executeid="Option 2">下拉列表项 2</li>
+                <li data-executeid="Option 3">下拉列表项 3</li>
             </template>
         </nue-dropdown>
     </nue-div>
 </template>
 
 <script setup lang="ts">
-function handleExecute(id: string) {
-    switch (id) {
-        case "profile":
-            NueMessage({ message: "Profile clicked" });
-            break;
-        case "settings":
-            NueMessage({ message: "Settings clicked" });
-            break;
-        case "logout":
-            NueMessage({ message: "Logout clicked" });
-            break;
-    }
-}
+const handleExecute = (id: string) => {
+    console.log(id);
+};
 </script>
