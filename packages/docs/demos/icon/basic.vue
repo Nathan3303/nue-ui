211b4d8e6e1a4562f426561e75522fe1f66293fc
Nathan3303 <Lee1928@outlook.com>
2024-08-29 22:35:29 +0800

    更新 Icon 组件文档 demos

--- packages/docs/demos/icon/basic.vue
+++ packages/docs/demos/icon/basic.vue
@@ -2,7 +2,7 @@
     <nue-div>
         <nue-icon name="search"></nue-icon>
         <nue-icon name="share"></nue-icon>
-        <nue-icon name="timer"></nue-icon>
-        <nue-icon name="connection"></nue-icon>
+        <nue-icon name="setting"></nue-icon>
+        <nue-icon name="subscribe"></nue-icon>
     </nue-div>
 </template>
--- packages/docs/demos/icon/color-size.vue
+++ packages/docs/demos/icon/color-size.vue
@@ -2,6 +2,6 @@
     <nue-div align="center">
         <nue-icon name="search" size="24px" color="red"></nue-icon>
         <nue-icon name="share" size="24px" color="#0F0"></nue-icon>
-        <nue-icon name="timer" size="24px" color="rgb(0, 0, 255)"></nue-icon>
+        <nue-icon name="setting" size="24px" color="rgb(0, 0, 255)"></nue-icon>
     </nue-div>
 </template>
