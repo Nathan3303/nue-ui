1a935baf02c1ee5bd705e017bb701a7f0885a0b2
Nathan3303 <Lee1928@outlook.com>
2024-09-10 22:31:22 +0800

    更新 Confirm 组件的 playground demo

--- packages/play/src/components/confirm.vue
+++ packages/play/src/components/confirm.vue
@@ -1,6 +1,9 @@
 <template>
     <nue-div>
         <nue-button @click="openConfirm">点击打开确认对话框</nue-button>
+        <nue-button @click="openConfirmWidthTheme">
+            点击打开确认对话框（带自定义主题）
+        </nue-button>
     </nue-div>
 </template>
 
@@ -18,6 +21,42 @@ function openConfirm() {
         () => NueMessage.info("操作取消!")
     );
 }
+
+function openConfirmWidthTheme() {
+    NueConfirm({
+        title: "确认对话框",
+        content: "你确定要删除吗？",
+        confirmButtonText: "确认",
+        cancelButtonText: "取消",
+        theme: "custom",
+    }).then(
+        () => NueMessage.success("删除确认!"),
+        () => NueMessage.info("操作取消!")
+    );
+}
 </script>
 
-<style scoped></style>
+<style>
+.nue-confirm--custom {
+    background-color: #c7ffff;
+
+    .nue-confirm__header * {
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    .nue-confirm__content * {
+        font-size: 14px;
+    }
+
+    .nue-confirm__footer {
+        .nue-button {
+            font-size: 14px;
+        }
+
+        .nue-button--primary {
+            background-color: var(--primary-color-a200);
+        }
+    }
+}
+</style>
