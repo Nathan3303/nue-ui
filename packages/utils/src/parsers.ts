2c366d1a76f392858ffd66ad579220f41184f6e9
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-12-03 14:44:11 +0800

    [patch] 增加 parseTheme 对对象类型的支持

--- packages/utils/src/parsers.ts
+++ packages/utils/src/parsers.ts
@@ -1,12 +1,18 @@
-import { isString } from './utils';
+import { isString } from 'lodash-es';
+import type { ObjectLikeThemes } from './types';
 
-export function parseTheme(value: string | string[], prefix: string): string[] {
+export function parseTheme(
+    value: string | string[] | ObjectLikeThemes,
+    prefix: string
+): string[] {
     const parseResult: string[] = [];
     let values: string[] = [];
     if (isString(value)) {
         values = (value as string).trim().split(',');
-    } else {
+    } else if (Array.isArray(value)) {
         values = value as string[];
+    } else {
+        values = Object.keys(value).filter(key => value[key]);
     }
     values = values.map(item => `${prefix}--${item.trim()}`);
     return parseResult.concat(values);
