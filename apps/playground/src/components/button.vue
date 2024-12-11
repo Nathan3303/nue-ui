4af34c656ec2eb17f84fc568cd3a27f91a1d042b
Nathan Lee <lee1928@outlook.com>
2024-12-11 13:24:04 +0800

    [patch] 更新 playground app 中的 Button 组件 demo

--- apps/playground/src/components/button.vue
+++ apps/playground/src/components/button.vue
@@ -9,42 +9,45 @@
         </nue-header>
         <nue-main>
             <nue-div style="padding: 16px">
-                <nue-button>Button</nue-button>
-                <nue-button icon="search">带图标的按钮</nue-button>
-                <nue-button disabled>禁用状态</nue-button>
-                <nue-button
-                    :loading="isSearching"
-                    loading-icon="scan"
-                    @click="handleSearch"
-                >
-                    加载状态以及自定加载图标
-                </nue-button>
-                <nue-button
-                    :throttle-duration="360"
-                    use-throttle
-                    @click="count++"
-                >
-                    节流 ({{ count }})
-                </nue-button>
-                <nue-button size="small">小按钮</nue-button>
-                <nue-button>正常按钮</nue-button>
-                <nue-button size="large">大按钮</nue-button>
-                <nue-button theme="round">圆角主题</nue-button>
-                <nue-button :theme="{ round: true, primary: true }">
-                    圆角主题+主要色主题
-                </nue-button>
-                <nue-button
-                    icon="search"
-                    theme="icon-only"
-                    title="仅图标主题"
-                />
-                <nue-button theme="pure">纯文本主题</nue-button>
+                <nue-text tag="h3">Basic</nue-text>
+                <nue-div align="center">
+                    <nue-button>Basic</nue-button>
+                    <nue-button icon="search">icon=search</nue-button>
+                    <nue-button disabled>disabled</nue-button>
+                    <nue-button
+                        :loading="isSearching"
+                        loading-icon="scan"
+                        @click="handleSearch"
+                    >
+                        loading & loading-icon
+                    </nue-button>
+                    <nue-button
+                        :throttle-duration="360"
+                        use-throttle
+                        @click="count++"
+                    >
+                        use-throttle & throttle-duration & {{ count }}
+                    </nue-button>
+                </nue-div>
+                <nue-div align="center">
+                    <nue-button size="small">size=small</nue-button>
+                    <nue-button>normal</nue-button>
+                    <nue-button size="large">size=large</nue-button>
+                </nue-div>
+                <nue-div align="center">
+                    <nue-button theme="round">theme=round</nue-button>
+                    <nue-button :theme="{ round: true, primary: true }">
+                        theme=round,primary
+                    </nue-button>
+                    <nue-button
+                        icon="search"
+                        theme="icon-only"
+                        title="仅图标主题"
+                    />
+                    <nue-button theme="pure">theme=pure</nue-button>
+                </nue-div>
                 <nue-button>
-                    <template #prepend>
-                        <nue-icon name="scan" />
-                    </template>
-                    带有 prepend 插槽、带有 append 插槽。当 NueButton
-                    组件文本过长时，会自动隐藏过长的文字并出现省略号。
+                    use-append-slot
                     <template #append>
                         <nue-icon name="scan" />
                     </template>
