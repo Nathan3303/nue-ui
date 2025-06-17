a1e20d0dcd85e17c60b824d0b1e58cb53a0a8689
Nathan3303 <Lee1928@outlook.com>
2025-06-17 23:56:29 +0800

    feat(histoire): 新增部分组件在 Histoire 的 Story 组件

--- apps/histoire/histoire.setup.ts
+++ apps/histoire/histoire.setup.ts
@@ -1,4 +1,5 @@
 import NueUI from 'nue-ui';
+import 'nue-ui/dist/index.css';
 
 export function setupVue3({ app }) {
     app.use(NueUI);
---
+++ apps/histoire/src/checkbox.story.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const checkbox1 = ref(false);
+</script>
+
+<template>
+    <Story title="NueCheckbox">
+        <nue-checkbox v-model="checkbox1" label="基础复选框" />
+    </Story>
+</template>
+
+<style scoped></style>
