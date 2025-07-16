3aa0859ab064f4cf506b67e2b8e5ac086d6c89c1
Natan3303 <lee1928@outlook.com>
2025-07-16 19:49:09 +0800

    layout(playground): 优化 Playground 组件演练界面排版

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <nue-container class="app-view">
         <nue-header>
-            <nue-div align="center" wrap="nowrap">
-                <nue-text size="lg">NueUI Playground</nue-text>
+            <nue-div flex="1">
                 <nue-link route="/component" theme="rl">组件演练</nue-link>
                 <nue-link route="/custom" theme="rl">自由演练</nue-link>
             </nue-div>
+            <nue-div width="fit-content">
+                <nue-text size="lg">NueUI Playground Demos</nue-text>
+            </nue-div>
+            <nue-div flex="1"></nue-div>
         </nue-header>
         <router-view />
     </nue-container>
