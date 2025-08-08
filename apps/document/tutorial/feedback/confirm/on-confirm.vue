ed56d3d5ca6dfd9fb8bb1f552dfa011a9fccf9f2
Natan3303 <lee1928@outlook.com>
2025-08-08 13:57:35 +0800

    update(document): 更新 Confirm 组件文档页 - onConfirm 回调函数修复

--- apps/document/tutorial/feedback/confirm/on-confirm.vue
+++ apps/document/tutorial/feedback/confirm/on-confirm.vue
@@ -5,33 +5,28 @@
 <script lang="ts" setup>
 import { NueConfirm, NueMessage } from 'nue-ui';
 
+const confirmHandler = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.random();
+            if (randomNumber > 0.5) {
+                resolve('成功');
+            } else {
+                reject('失败，请重试');
+            }
+        }, 1024);
+    });
+};
+
 const openConfirmWithOnConfirm = () => {
     NueConfirm({
         title: '你确定要继续吗？',
         content: '执行此操作将无法撤销。这将永久删除您的帐户，并从我们的服务器上清除您的数据。',
         confirmButtonText: '继续',
         cancelButtonText: '不继续',
-        onConfirm: () => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    const randomNumber = Math.random();
-                    if (randomNumber > 0.5) {
-                        resolve('成功');
-                    } else {
-                        reject('失败，请重试');
-                    }
-                }, 1024);
-            });
-        }
-    }).then(
-        res => NueMessage.success(res as string),
-        err => {
-            if (err instanceof Error) {
-                NueMessage.error(err.message);
-            } else {
-                NueMessage.info('操作取消!');
-            }
-        }
-    );
+        onConfirm: async () => await confirmHandler()
+    })
+        .then((res: string) => NueMessage.success(res as string))
+        .catch((err: Error) => NueMessage.error(err.message));
 };
 </script>
