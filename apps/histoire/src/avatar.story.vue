12e56bad7e9417f6e3ff0e5c0f3d6a9818ecc288
Nathan <lee1928@outlook.com>
2026-04-01 13:39:16 +0800

    fix: 移除 Historie App 并调整 .gitignore 文件

--- apps/histoire/src/avatar.story.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <Story :layout="{ type: 'grid', width: '96%' }" title="NueAvatar">
-        <nue-avatar></nue-avatar>
-    </Story>
-</template>
-
-<style scoped></style>
